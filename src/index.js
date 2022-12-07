5d7d60ad5ccb6ec1be0d6a576df84ac905165f3b
Vincent Moittié <moittie.vincent@gmail.com>
2022-12-07 12:52:59 +0100

    Update default class

--- src/index.js
+++ src/index.js
@@ -1,27 +1,3 @@
-import MyPluginDrawer from './draw/MyPluginDrawer';
-import MyPluginMetadata from './metadata/MyPluginMetadata';
-import MyPluginParser from './parser/MyPluginParser';
-import MyPluginRenderer from './render/MyPluginRenderer';
-import resources from './assets';
+import GithubActionPlugin from 'src/models/GithubActionPlugin';
 
-/*
- * your plugins, for instance :
- *
- * import MyPluginDrawer from './draw/MyPluginDrawer';
- * will be replaced by
- * import [NameOfYourPlugin]Drawer from './draw/[NameOfYourPlugin]Drawer';
- *
- * Leto-modelizer expects to receive from this plugin an Object containing those properties:
- * - PluginDrawer
- * - PluginMetadata
- * - PluginParser
- * - PluginRenderer
- * - resources
- */
-export default {
-  PluginDrawer: MyPluginDrawer,
-  PluginMetadata: MyPluginMetadata,
-  PluginParser: MyPluginParser,
-  PluginRenderer: MyPluginRenderer,
-  resources,
-};
+export default GithubActionPlugin;
